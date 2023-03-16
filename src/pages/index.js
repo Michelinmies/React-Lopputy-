2939c32b42507957e66dc7a301559c75782fef62
Michelinmies <niku.riekko@gmail.com>
2023-03-16 19:22:18 +0200

    https://github.com/Michelinmies/meetup-app.git

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,5 @@
+import { MongoClient } from 'mongodb';
 import MeetupList from '../components/meetups/MeetupList';
-
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup: Vaskiluoto',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/0/02/Vasklot_hamn_cirka_1994_med_Silja_Festival_p%C3%A5_inkommande_och_Wasa_Queen_vid_kaj_i_bakgrunden_Sundom_sk%C3%A4rg%C3%A5rd.jpg',
-    address: 'Vaskiluoto, Vaasa',
-    description: 'This is a first meetup! Picture: Maritime Museum of Finland, CC BY-SA 4.0',
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup: VAMK',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/8/88/Vaasan_ammattikorkeakoulu_2018.jpg',
-    address: 'Wolffintie 30, Vaasa',
-    description: 'This is a second meetup! Picture: Santeri Viinamäki, 2018m CC BY-SA 4.0',
-  },
-];
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
@@ -25,12 +7,38 @@ function HomePage(props) {
 
 export async function getStaticProps() {
   // fetch data from an API
+  const client = await MongoClient.connect(
+    'mongodb+srv://meetupuser:RKbhVj8zhkbjivJy@cluster0.fzf8u3l.mongodb.net/meetups_db?retryWrites=true&w=majority'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
-    revalidate: 1
+    revalidate: 1,
   }; 
 }
 
+/* export async function getStaticProps() {
+    // fetch data from API
+    return {
+      props: {
+        meetups: DUMMY_MEETUPS
+      };
+      revalidate: 1
+    };
+}
+  */
 export default HomePage;
