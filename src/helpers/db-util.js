e3e86d18f877f1cfdf358fd4e35484b5f101dad5
Michelinmies <niku.riekko@gmail.com>
2023-03-18 18:17:14 +0200

    Päivitys

--- src/helpers/db-util.js
+++ src/helpers/db-util.js
@@ -24,4 +24,19 @@ export async function getAllDocuments(client, collection, sort, filter = {}) {
     return documents;
 }
 
+export async function getDocumentIdList(client, collection) {
+    const db = client.db();
+    const idList = await db.collection(collection).find({}, { _id: 1 }).toArray();
+
+    return idList;
+}
+
+export async function getOneDocument(client, collection, filter = {}) {
+    const db = client.db();
+
+    const document = await db.collection(collection).findOne(filter);
+
+    return document;
+}
+
 
--- src/pages/[meetupId]/index.js
+++ src/pages/[meetupId]/index.js
@@ -1,50 +1,73 @@
+// import only packets that are needed
+import { ObjectId } from 'mongodb';
+import { Fragment } from 'react';
+import Head from 'next/head';
 import MeetupDetail from '../../components/meetups/MeetupDetail';
+import { connectDatabase, getDocumentIdList, getOneDocument } from '../../helpers/db-util';
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
+    <Fragment>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta name='description' content={props.meetupData.description} />
+    </Head> 
     <MeetupDetail
-      image='https://upload.wikimedia.org/wikipedia/commons/8/88/Vaasan_ammattikorkeakoulu_2018.jpg'
-      title='TEMP PLACEHOLDER: First Meetup'
-      address='TEMP PLACEHOLDER: Some Street 5, Some City'
-      description='TEMP PLACEHOLDER: This is a meetup'
-    />
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+      />
+    </Fragment>       
   );
 }
 
 export async function getStaticPaths() {
+  // fetch id: s for all meetups
+
+  let client;
+  try {
+    client = await connectDatabase();
+  } catch (error) {
+    console.log("Error connectin to MongoDB: ", error);
+  }
+  
+  let meetups;
+
+  try {
+    meetups = await getDocumentIdList(client, 'meetups');
+    client.close();
+  } catch (error) {
+    console.log("Error getting meetup list:", error);
+  }
+
   return {
-    fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: 'm1',
-        },
-      },
-      {
-        params: {
-          meetupId: 'm2',
-        },
-      },
-    ],
+    fallback: 'blocking',
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 }
-
+  
 export async function getStaticProps(context) {
   // fetch data for a single meetup
 
   const meetupId = context.params.meetupId;
 
-  console.log(meetupId);
+  // TODO: add try/catch blocks
+  const client = await connectDatabase();
+  const meetupIdObj = new ObjectId(meetupId);
+  const selectedMeetup = await getOneDocument(client, 'meetups', {_id : meetupIdObj } );
+  client.close();
 
   return {
     props: {
       meetupData: {
-        image:
-          'https://upload.wikimedia.org/wikipedia/commons/8/88/Vaasan_ammattikorkeakoulu_2018.jpg',
-        id: meetupId,
-        title: 'TEMP PLACEHOLDER:First Meetup',
-        address: 'TEMP PLACEHOLDER:Some Street 5, Some City',
-        description: 'TEMP PLACEHOLDER: This is a meetup',
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
       },
     },
   };
